528e3aa06af98090726e5974b17ee0242fcccff0
Esraa <esraa.ramadan667@gmail.com>
2023-11-28 18:30:13 +0200

    just sent and get

---
+++ android/app/src/main/kotlin/com/example/note_app52/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.note_app52
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+   sendData(){
+     firebaseFirestore.collection("notes").add(
+         {
+           'note' : noteController.text
+         }).then((value) {
+           print(value);
+        });
+   }
+
+   getData(){
+     firebaseFirestore.collection("notes").snapshots();
+   }
+
+
+var noteController =TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: noteController,
+              decoration: InputDecoration(
+                hintText: " enter your note "
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+              sendData();
+            }, child: Text("Sent")),
+            Divider(thickness: 1,),
+            StreamBuilder<QuerySnapshot>
+              (stream: firebaseFirestore.collection("notes").snapshots(),
+                builder: (context, snapshot){
+                return snapshot.hasData ? snapshot.data!.docs.length !=0 ?
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index){
+                      return Center(
+                      child:  Text(
+                         snapshot.data!.docs[index]['note']
+                        )
+                      );
+                    }):
+                    Center(child:  Text("no data"),) :
+                    snapshot.hasError ? Center(child:  Text("error happend"),):
+                    Center(
+                      child: CircularProgressIndicator(),
+                    );
+
+                })
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
